6275a0824d9f847f33a4e9c9eff187111c8be268
Andrew1495 <andrewfairley95@gmail.com>
2022-04-18 16:59:18 +0100

    finshed java script

--- java.js
+++ java.js
@@ -1,10 +1,70 @@
 
 const DEFAULT_SIZE = 16;
-const etch =document.querySelector(`#etch`);
-const clear=document.querySelector(`#clear`);
+const DEFAULT_COLOR = `#333333`;
+const DEFAULT_MODE = `normal`;
+
+let currentMode = DEFAULT_MODE;
 let size = DEFAULT_SIZE;
 
+const etch =document.querySelector(`#etch`);
+const clear=document.querySelector(`#clear`);
+const eraseButton=document.querySelector(`#erase`);
+const rainbowButton=document.querySelector(`#rainbow`);
+const normalColorButton=document.querySelector(`#normal`);
+const slider=document.querySelector(`#slider`);
+const newGrid =document.querySelector(`#change`);
+
+
+
+
+
 gridSize(size);
+
+let mouseDown = false;
+document.body.onmousedown = () =>(mouseDown=true);
+document.body.onmouseup = () =>(mouseDown=false);
+
+
+
+clear.addEventListener(`click`,()=>{ //on click clears grid
+    gridClear(size);
+})
+normalColorButton.addEventListener(`click`,()=>{
+    currentMode = `normal`
+    activeButton()
+    changeColor()
+})
+
+
+
+eraseButton.addEventListener(`click`,()=>{
+    currentMode = `erase`
+    activeButton()
+    changeColor()
+})
+
+
+rainbowButton.addEventListener(`click`,()=>{
+    currentMode = `rainbow`
+    activeButton()
+    changeColor()
+})
+
+
+
+slider.addEventListener(`change`,()=>{
+console.log(slider.value);
+size = slider.value;
+const sizeVal = document.querySelector(`#sizeval`);
+sizeVal.textContent = size;
+
+})
+
+
+newGrid.addEventListener(`click`,()=>{
+    gridDelete(size)
+    gridSize(size);
+})
 
 
 function gridSize(size){   //defines grid size and lets user color it
@@ -13,23 +73,61 @@ function gridSize(size){   //defines grid size and lets user color it
         for(x=0; x<(size*size);x++){
         const grid = document.createElement(`div`);
         grid.classList.add(`grid`);
-        etch.appendChild(grid);
-        grid.addEventListener(`mouseover`,() => {
-        grid.setAttribute(`style`,`background-color: black;`)
-})}}
+        grid.addEventListener(`mouseover`,changeColor)
+        grid.addEventListener(`mousedown`,changeColor)
+         etch.appendChild(grid);
+        }
+}
 
 
 
-clear.addEventListener(`click`,()=>{ //on click clears grid
-    gridClear(size);
-})
 
-
-function gridClear(size){ // clears grid and make a ne wone
+function gridClear(size){ // clears grid and make a new one
     while (etch.hasChildNodes()) {
         etch.removeChild(etch.firstChild);
       }
       gridSize(size);
 }
+
+function gridDelete(size){
+while (etch.hasChildNodes()) {
+    etch.removeChild(etch.firstChild);
+  }
+}
+
+
+function changeColor(){
+    const grid= document.querySelector(`#grid`);
+    if (mouseDown !== true)
+        return;
+    else if(currentMode === `erase`){
+    this.style.backgroundColor = `white`;
+    }
+    else if(currentMode === `normal`){
+    this.style.backgroundColor = `black`;
+    }
+    else if (currentMode === `rainbow`){
+        let randomColor = Math.floor(Math.random()*1677215).toString(16);
+    this.style.backgroundColor ="#" + randomColor;
+    }
+}
+
+function activeButton(){
+    if(currentMode === `normal`){
+        normalColorButton.classList.remove(`active`);
+    }else if (currentMode === `erase`){
+        eraseButton.classList.remove(`active`);
+    }else if (currentMode=== `rainbow`){
+        rainbowButton.classList.remove(`active`);
+    }
+    if(currentMode === `normal`){
+        normalColorButton.classList.add(`active`)
+    }else if (currentMode === `erase`){
+        eraseButton.classList.add(`active`);
+    }else if (currentMode=== `rainbow`){
+        rainbowButton.classList.add(`active`);
+    }
+}
+
 
 
